00f88917d1bb02874350e36d621e57795346524b
Vsevolod Petrov <vsev92@mail.ru>
2024-08-12 16:12:00 +0700

    install phpcs

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ *@category tag in file comment
+ *@package tag in file comment
+ *@author tag in file comment
+ *@license tag in file comment
+ *@link tag in file comment
+ * Description of what this module (or file) is doing.
+ */
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Файл не включается напрямую
